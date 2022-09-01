cb73932291739b758a7c9d095c06a6c280c3bde1
tatiankris <tatiankris@mail.ru>
2022-09-01 18:29:09 +0300

    fixed bug with pseudo-class before

--- src/works/work/Work.js
+++ src/works/work/Work.js
@@ -4,21 +4,16 @@ import button from '../../common/styles/Button.module.scss';
 
 const Work = (props) => {
     return <div className={styles.work}>
-        <div className={styles.workImage}>
-                <div className={styles.workImg} style={props.style}/>
-                <a href={'#'} className={`${button.button} ${styles.butt}`}>Look</a>
-        </div>
+                <div className={styles.workImg} style={props.style}>
+                    <a href={'#'} className={`${button.button} ${styles.butt}`}>Look</a>
+                </div>
 
-        <div className={styles.workDescription}>
-            <div className={styles.descriptionTitle}>
-                <div>{props.title}</div>
-            </div>
-            <div className={styles.description}>
-                {props.description}
-            </div>
-        </div>
-
-
+                <div className={styles.workDescription}>
+                    <h3 className={styles.descriptionTitle}>{props.title}</h3>
+                    <p className={styles.description}>
+                        {props.description}
+                    </p>
+                </div>
     </div>
 
 }
